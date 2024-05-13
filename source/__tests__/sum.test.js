cacc21ce6e99ef4f9fa79d7dc00d03f563c55d8c
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2024-05-12 21:04:03 -0700

    add demo files

--- source/__tests__/sum.test.js
+++ source/__tests__/sum.test.js
@@ -1,3 +1,9 @@
+const sum = require('../scripts/sum');
+
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
+});
+
+test('sum test', () => {
+    expect(sum(1, 2)).toBe(3);
 });
---
+++ source/scripts/sum.js
@@ -0,0 +1,11 @@
+/**
+ * A simple function that adds two numbers together
+ * @param {number} a the first number
+ * @param {number} b the second number
+ * @returns the sum of a and b
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum;
