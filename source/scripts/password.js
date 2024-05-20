1dbcc73aa5daed30e35213a8a18f9966b6b7bbf1
matt0923 <74699880+matt0923@users.noreply.github.com>
2024-05-20 13:46:03 -0700

    Added help box functionality (layering is messed up though)

--- source/scripts/password.js
+++ source/scripts/password.js
@@ -19,4 +19,22 @@ async function init() {
             homePage.click();
         }
     });
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("triggerPopup");
+    var span = document.getElementsByClassName("close")[0]; 
+    
+    img.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
