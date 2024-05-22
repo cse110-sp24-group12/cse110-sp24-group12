2cdcb1bdbcc576c93fbf5730488dcc901fface80
lindseyrapp <84059264+lindseyrapp@users.noreply.github.com>
2024-05-22 14:39:58 -0700

    Update preload.js to resolve conflicts

--- source/scripts/preload.js
+++ source/scripts/preload.js
@@ -26,18 +26,16 @@ window.addEventListener('DOMContentLoaded', () => {
  * This function defines the API that will be exposed to the renderer process.
  * @memberof Preload
  */
-contextBridge.exposeInMainWorld(
-'api', {
-        writeFile: (args) => ipcRenderer.send('write-file', args),
-        readMarkdown: (args) => ipcRenderer.send('read-markdown', args),
-        onReadMarkdownReply: (callback) => ipcRenderer.on('read-markdown-reply', (event, args) => callback(args)),
-        writePassword: (args) => ipcRenderer.invoke('write-password', args),
-        readPassword: (args) => ipcRenderer.invoke('read-password',args),
-        readFile: (args) => ipcRenderer.invoke('read-file', args),
-        renderMarkdown: (args) => ipcRenderer.invoke('render-markdown', args),
-        getEntryByMonth: (args) => ipcRenderer.invoke('get-entries-for-month', args),
-        getEntryById: (args) => ipcRenderer.invoke('get-entry-by-id', args),
-        addMarkdownEntry: (args) => ipcRenderer.invoke('add-markdown-entry', args),
-        updateMarkdownEntry: (args) => ipcRenderer.invoke('update-markdown-entry', args),
-    }
-);
+contextBridge.exposeInMainWorld('api', {
+    writeFile: (args) => ipcRenderer.send('write-file', args),
+    readMarkdown: (args) => ipcRenderer.send('read-markdown', args),
+    onReadMarkdownReply: (callback) => ipcRenderer.on('read-markdown-reply', (event, args) => callback(args)),
+    writePassword: (args) => ipcRenderer.invoke('write-password', args),
+    readPassword: (args) => ipcRenderer.invoke('read-password', args),
+    readFile: (args) => ipcRenderer.invoke('read-file', args),
+    renderMarkdown: (args) => ipcRenderer.invoke('render-markdown', args),
+    getEntryByMonth: (args) => ipcRenderer.invoke('get-entries-for-month', args),
+    getEntryById: (args) => ipcRenderer.invoke('get-entry-by-id', args),
+    addMarkdownEntry: (args) => ipcRenderer.invoke('add-markdown-entry', args),
+    updateMarkdownEntry: (args) => ipcRenderer.invoke('update-markdown-entry', args)
+});
