a73e77e52a4d36a2a771271227ead1ca33d46dd5
Ramtin Tajbakhsh <69605985+RamtinTJB@users.noreply.github.com>
2024-06-01 15:44:51 -0700

    fixed page flow (#86)
    * fixed page flow
    
    * fix e2e tests

--- source/scripts/preload.js
+++ source/scripts/preload.js
@@ -37,4 +37,5 @@ contextBridge.exposeInMainWorld('api', {
   getEntryById: (args) => ipcRenderer.invoke('get-entry-by-id', args),
   addMarkdownEntry: (args) => ipcRenderer.invoke('add-markdown-entry', args),
   updateMarkdownEntry: (args) => ipcRenderer.invoke('update-markdown-entry', args),
+  loadHtmlFile: (args) => ipcRenderer.invoke('load-html-file', args),
 });
